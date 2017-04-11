82d5e53205a2d2f6858175a03fc093e898ed7319
Adrian <vezoriousaj@gmail.com>
2017-04-11 16:25:48 -0400

    harpoon functionality stuff

--- src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoon.java
+++ src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoon.java
@@ -1,33 +1,78 @@
 package ValkyrienWarfareCombat.Entity;
 
+import java.util.Random;
+
+import ValkyrienWarfareBase.API.Vector;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
-public class EntityHarpoon extends Entity{
+public class EntityHarpoon extends EntityArrow{
 
-	public EntityHarpoon(World worldIn) {
+	//TODO make below two vars configurable
+	public static final int MAX_DMG = 20;//10 hearts
+	public static final int MIN_DMG = 5;//2.5 hearts
+
+	private EntityHarpoonGun origin;
+
+	private EntityHarpoon(World worldIn) {
 		super(worldIn);
 	}
 
-	@Override
-	protected void entityInit() {
-		// TODO Auto-generated method stub
-		
+	public EntityHarpoon(World worldObj, Vector velocityVector, EntityHarpoonGun origin){
+		super(worldObj);
+		this.origin=origin;
+		this.setVelocity(velocityVector.X, velocityVector.Y, velocityVector.Z);
 	}
 
 	@Override
-	protected void readEntityFromNBT(NBTTagCompound compound) {
-		// TODO Auto-generated method stub
-		
+	public void onHit(RayTraceResult raytrace){
+
+		Entity entity = raytrace.entityHit;
+
+		if (entity == null){
+			
+			raytrace.getBlockPos();
+			
+		}else{//it hit an entity
+
+			DamageSource damagesource;
+
+			if (shootingEntity == null){
+				damagesource = DamageSource.causeArrowDamage(this, this);
+			}else{
+				damagesource = DamageSource.causeArrowDamage(this, shootingEntity);
+			}
+
+			if (entity.attackEntityFrom(damagesource, new Random().nextInt(MAX_DMG+1-MIN_DMG)+MIN_DMG)){
+
+				if (entity instanceof EntityLivingBase){
+
+					EntityLivingBase liveEnt = (EntityLivingBase) entity;
+
+					if (shootingEntity instanceof EntityLivingBase){
+						EnchantmentHelper.applyThornEnchantments(liveEnt, shootingEntity);
+						EnchantmentHelper.applyArthropodEnchantments(liveEnt, shootingEntity);
+					}
+
+					arrowHit(liveEnt);
+
+
+					this.setDead();
+				}
+			}
+		}
+
 	}
 
 	@Override
-	protected void writeEntityToNBT(NBTTagCompound compound) {
-		// TODO Auto-generated method stub
-		
+	protected ItemStack getArrowStack(){
+		return null;
 	}
-	
-	
-	
+
 }
--- src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoonGun.java
+++ src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoonGun.java
@@ -3,84 +3,114 @@ package ValkyrienWarfareCombat.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import ValkyrienWarfareBase.API.RotationMatrices;
+import ValkyrienWarfareBase.API.Vector;
+import ValkyrienWarfareBase.PhysicsManagement.PhysicsWrapperEntity;
 import ValkyrienWarfareCombat.ValkyrienWarfareCombatMod;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class EntityHarpoonGun extends EntityMountingWeaponBase{
 
-	long lastFire = -Long.MIN_VALUE;
+	//TODO make below two vars configurable
+	private final static int POWDER_NEEDED = 4;//amount of gunpowder needed per s0hot
+	private final static int TIME_COOLDOWN_TICKS = 60;//3 sec
 
-	private boolean loaded = false;
-	private boolean powdered = false;
-
-	private final static int POWDER_NEEDED = 4;
+	private long lastFireTick = -TIME_COOLDOWN_TICKS;
 
 	public EntityHarpoonGun(World worldIn){
 		super(worldIn);
 	}
 
-	public void fire(EntityPlayer actor, ItemStack ammo, EnumHand hand){
+	public void fire(EntityPlayer actor, ItemStack stack, EnumHand hand){
 
-		
+		Vec3d velocityNormal = getVectorForRotation(rotationPitch, rotationYaw);
+		Vector velocityVector = new Vector(velocityNormal);
 
-		loaded = false;
+		PhysicsWrapperEntity wrapper = getParentShip();
+		if(wrapper != null){
+			RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWRotation, velocityVector);
+		}
+
+		velocityVector.multiply(2D);
+		EntityHarpoon projectile = new EntityHarpoon(worldObj, velocityVector, this);
+
+		projectile.shootingEntity = actor;
+
+		Vector projectileSpawnPos = new Vector(0,.5,0);
+
+		if(wrapper != null){
+			RotationMatrices.applyTransform(wrapper.wrapping.coordTransform.lToWRotation, projectileSpawnPos);
+		}
+
+		projectile.posX += projectileSpawnPos.X;
+		projectile.posY += projectileSpawnPos.Y;
+		projectile.posZ += projectileSpawnPos.Z;
+
+		worldObj.spawnEntityInWorld(projectile);
+
+		lastFireTick = getServer().getTickCounter();
 	}
 
 	@Override
 	public void onRiderInteract(EntityPlayer player, ItemStack stack, EnumHand hand){
 
-		ItemStack harpoon = null;
-		List<ItemStack> gunpowders = new ArrayList<ItemStack>();
-		int gunpowderSum = 0;
+		if (!player.worldObj.isRemote){
+			if (getServer().getTickCounter()-lastFireTick >= TIME_COOLDOWN_TICKS){
 
-		for (ItemStack[] inv : player.inventory.allInventories){
-			for (ItemStack is : inv){
+				ItemStack harpoon = null;
+				List<ItemStack> gunpowders = new ArrayList<ItemStack>();
+				int gunpowderSum = 0;
 
-				if (is == null)
-					continue;
+				for (ItemStack[] inv : player.inventory.allInventories){
+					for (ItemStack is : inv){
 
-				if (is.getItem().equals(ValkyrienWarfareCombatMod.instance.harpoon)){
-					if (harpoon == null)
-						harpoon = is;
-				}
+						if (is == null)
+							continue;
 
-				else if (is.getItem().equals(Items.GUNPOWDER)){
-					gunpowderSum+=is.stackSize;
-					gunpowders.add(is);
-				}
+						if (is.getItem().equals(ValkyrienWarfareCombatMod.instance.harpoon)){
+							if (harpoon == null)
+								harpoon = is;
+						}
 
-				if (harpoon != null){
-					if (gunpowderSum >= POWDER_NEEDED){
+						else if (is.getItem().equals(Items.GUNPOWDER)){
+							gunpowderSum+=is.stackSize;
+							gunpowders.add(is);
+						}
 
-						gunpowderSum=POWDER_NEEDED;//this and the for loop for removing necessary amount of gunpowder
-						for (ItemStack s : gunpowders){
-							s.stackSize=Math.max(s.stackSize-gunpowderSum,0);
-							if (s.stackSize<=0){
-								int index = player.inventory.getSlotFor(s);
-								player.inventory.setInventorySlotContents(index, null);
+						if (harpoon != null){
+							if (gunpowderSum >= POWDER_NEEDED){
+
+								gunpowderSum=POWDER_NEEDED;//this and the for loop for removing necessary amount of gunpowder
+								for (ItemStack s : gunpowders){
+									s.stackSize=Math.max(s.stackSize-gunpowderSum,0);
+									if (s.stackSize<=0){
+										int index = player.inventory.getSlotFor(s);
+										player.inventory.setInventorySlotContents(index, null);
+									}
+								}
+								gunpowders = null;
+
+								harpoon.stackSize--;
+								if (harpoon.stackSize<=0){
+									int index = player.inventory.getSlotFor(harpoon);
+									player.inventory.setInventorySlotContents(index, null);
+								}
+
+								harpoon = null;
+
+								fire(player,stack,hand);
+								return;//cancel the loop
 							}
 						}
-						gunpowders = null;
 
-						harpoon.stackSize--;
-						if (harpoon.stackSize<=0){
-							int index = player.inventory.getSlotFor(harpoon);
-							player.inventory.setInventorySlotContents(index, null);
-						}
-
-						harpoon = null;
-
-						fire(player,stack,hand);
-						return;//cancel the loop
 					}
 				}
-
 			}
-
 		}
 	}
 
