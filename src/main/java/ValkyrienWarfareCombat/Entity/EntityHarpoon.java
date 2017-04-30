0effa01b36e03f41260ba1f7c8b05c6b3e50e1ea
Nimbleguy <nomblest@gmail.com>
2017-04-29 23:41:12 -0400

    literally typewriter monkey

--- src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoon.java
+++ src/main/java/ValkyrienWarfareCombat/Entity/EntityHarpoon.java
@@ -2,6 +2,7 @@ package ValkyrienWarfareCombat.Entity;
 
 import java.util.Random;
 
+import ValkyrienWarfareBase.API.ValkyrienWarfareHooks;
 import ValkyrienWarfareBase.API.Vector;
 import ValkyrienWarfareCombat.ValkyrienWarfareCombatMod;
 import ValkyrienWarfareCombat.Network.PacketHarpoon;
@@ -58,6 +59,9 @@ public class EntityHarpoon extends EntityArrow{
 		if(entity == null){//its a block
 			if(this.motionX != 0 && this.motionY != 0 && this.motionZ != 0 && !this.worldObj.isRemote){
 				ValkyrienWarfareCombatMod.instance.network.sendToAll(new PacketHarpoon(this.origin.getEntityId(), this.getEntityId()));
+			}
+			if(ValkyrienWarfareHooks.isBlockPartOfShip(this.worldObj, raytrace.getBlockPos())){
+				this.startRiding(ValkyrienWarfareHooks.getShipEntityManagingPos(this.worldObj, raytrace.getBlockPos()));
 			}
 		}
 		else if(!(entity.equals(origin) || entity.equals(this))){//it hit an entity that isn't a harpoon gun
